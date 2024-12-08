622715bd4bca359deb1fb5635c7a475b79e0b77e
lala2661 <aegraves6@gmail.com>
2024-12-07 16:16:59 -0800

    Now can add tasks

--- tasks.js
+++ tasks.js
@@ -1,5 +1,14 @@
 let addTaskBtn = document.getElementById("add-task");
+let taskContainer = document.getElementById("task-container");
+let task = document.getElementsByClassName("task")[1];
 
 addTaskBtn.addEventListener("click", (e) => {
-    console.log("pressed")
+    const newTask = document.createElement("li");
+    newTask.className = "task";
+    const newInput = document.createElement("input");
+    newInput.value = "your new quest";
+    newTask.appendChild(newInput);
+    taskContainer.appendChild(newTask);
+
+
 })
